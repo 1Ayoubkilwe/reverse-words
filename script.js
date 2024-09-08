e59411d780038b4cd572536fe54c39afef3fe67b
Ayoub Jama Khalid <buuyacamaajdilaakh@gmail.com>
2024-09-08 11:36:19 +0300

    jawabahyga

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 // Good Luck 💪🏾
+function reverseWords(sentence) {
+    return sentence.split(' ').reverse().join(' ');
+  }
+  
+  console.log(reverseWords("fikrcamp bootcamp")); // "bootcamp fikrcamp"
+  console.log(reverseWords("hello there!")); // "there! hello"
+  console.log(reverseWords("welcome to my city")); // "city my to welcome"
+  
